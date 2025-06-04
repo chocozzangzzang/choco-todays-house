9b5fa6f489d1c817537e7672b64bb4f926f7e0ba
chocozzang <jangjh_05@naver.com>
2025-06-04 17:41:13 +0900

    json modify

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from './ProductCard';
 
 const MainPage = () => {
+    const [ products, setProducts ] = useState([]);
+
+    const getProducts = async() => {
+        let url = "https://my-json-server.typicode.com/chocozzangzzang/choco-todays-house/products";
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
     return (
-        <div>MainPage</div>
+        <div className='products-page'>
+            {
+                products.map( (product) => <ProductCard product={product}/> )
+            }
+        </div>
     )
 }
 
